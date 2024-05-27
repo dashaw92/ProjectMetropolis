32955d635bc157cb61c552f33c01ff27b5b071e3
dashaw92 <dashaw92@gmail.com>
2024-05-27 09:43:27 -0400

    Initial Scavs have a configurable chance of spawning with the bombs Custom item weighting hook added to prioritize bombs in combat Config menu is WIP, just the bare minimum to ensure odds worked.

---
+++ ProjectMetropolis/ProjMetropolisOptionsMenu.cs
@@ -0,0 +1,31 @@
+﻿using Menu.Remix.MixedUI;
+using UnityEngine;
+
+namespace ProjectMetropolis {
+    public class ProjMetropolisOptionsMenu : OptionInterface {
+        private readonly ProjectMetropolis _mod;
+        public readonly Configurable<int> ReplacementChance;
+
+        public ProjMetropolisOptionsMenu(ProjectMetropolis mod) {
+            _mod = mod;
+            
+            ReplacementChance = config.Bind("replacementChance", 1);
+        }
+
+        public override void Initialize() {
+            base.Initialize();
+            
+            var tab = new OpTab(this, "Options");
+            Tabs = new[] { tab };
+            var options = new UIelement[] {
+                new OpLabel(10f, 550f, "Config", true),
+                new OpSlider(ReplacementChance, new Vector2(10f, 590f), 100)
+                {
+                    description = "Configure the chances scavengers will spawn with a singularity bomb"
+                }
+            };
+            
+            tab.AddItems(options);
+        }
+    }
+}
---
+++ ProjectMetropolis/ProjectMetropolis.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using BepInEx;
+using MoreSlugcats;
+
+namespace ProjectMetropolis {
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class ProjectMetropolis : BaseUnityPlugin {
+        public const string PluginGuid = "dashaw92.projectmetropolis";
+        public const string PluginName = "The Metropolis Project";
+        public const string PluginVersion = "0.0.1";
+
+        private readonly ProjMetropolisOptionsMenu _options;
+        
+        public ProjectMetropolis() {
+            _options = new ProjMetropolisOptionsMenu(this);
+        }
+
+        public void OnEnable() {
+            On.RainWorld.OnModsInit += RWModInit;
+            On.ScavengersWorldAI.AddScavenger += ScavengerSpawnHook;
+            On.ScavengerAI.WeaponScore += ScavengerAIOnWeaponScore;
+        }
+
+        private int ScavengerAIOnWeaponScore(On.ScavengerAI.orig_WeaponScore orig, ScavengerAI self, PhysicalObject obj, bool pickupdropinsteadofweaponselection) {
+            return obj is SingularityBomb ? 6 : orig(self, obj, pickupdropinsteadofweaponselection);
+        }
+
+        private void RWModInit(On.RainWorld.orig_OnModsInit orig, RainWorld self) {
+            orig(self);
+            try {
+                MachineConnector.SetRegisteredOI(PluginGuid, _options);
+            }
+            catch (Exception) {
+                Logger.LogWarning($"Failed to register OI for {PluginGuid}");
+            }
+        }
+        
+        private void ScavengerSpawnHook(On.ScavengersWorldAI.orig_AddScavenger orig, ScavengersWorldAI self, ScavengerAbstractAI scav) {
+            if (UnityEngine.Random.Range(0, 100) < _options.ReplacementChance.Value) {
+                var bomb = new AbstractPhysicalObject(self.world, MoreSlugcatsEnums.AbstractObjectType.SingularityBomb,
+                    null, scav.parent.pos, self.world.game.GetNewID());
+                self.world.GetAbstractRoom(scav.parent.pos).AddEntity(bomb);
+                _ = new AbstractPhysicalObject.CreatureGripStick(scav.parent, bomb, 0, true);
+            }
+
+            orig(self, scav);
+        }
+    }
+}
